a1b4e535cc49cb9df2ca1a5e7e67e35f96829c00
MarcoRob <marcko.rp@gmail.com>
2018-02-03 13:59:00 -0600

    Initial React Setup

---
+++ src/components/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+export default class Hello extends React.Component<any, any> {
+
+    public render() {
+        return (
+            <h1>Hello world!</h1>
+        );
+    }
+
+}
