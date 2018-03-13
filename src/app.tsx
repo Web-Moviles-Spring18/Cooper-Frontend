c9a44317c701355367c7182868a5082b3ea132dd
MarcoRob <marcko.rp@gmail.com>
2018-03-13 17:25:41 -0600

    Setting up BrowserHistory react-router

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Login from "./components/Login";
+import Home from "./components/Home";
+ 
 
-ReactDOM.render(<Login />, document.getElementById("app"));
+ReactDOM.render(
+    <Router>
+        <div>            
+            <Route exact path="/" component={ Home } />
+            <Route path="/login" component={ Login }/>
+        </div>
+    </Router>
+    , document.getElementById("app"));
 
