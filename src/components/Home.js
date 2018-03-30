09cf613e67eedeb74c5037d0d5c011e31768828f
Neymgm <neymgm@mac.com>
2018-03-30 16:20:00 -0600

    Add Partners section and modify layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,31 +10,32 @@ export default class Home extends Component {
 
         <section className="section">
           {/* About */}
-          <section className="hero is-bold">
-            <div className="hero-body">
-              <div className="container is-fluid">
-                <div className="columns">
-                  {/* Hero Body */}
-                  <div className="column is-6">
-                    <div className="content">
-                      <h1 className="title">
-                        What is Cooper?
+          <section className="hero is-medium is-bold">
+            <div className="section">
+              <div className="content">
+                <h1 className="title has-text-centered">
+                  What is Cooper?
                   </h1>
-                      <h4 className="subtitle">
-                        Cooper is the best way to <strong> share and collect </strong> money from friends and family!
-                  </h4>
-                      <p>
-                        Wether you're spliting the check for a pizza, the the movies or even a weekend trip,
-                          with Cooper you can send out a request for any amount and get rid of all the hassles
-                </p>
-                    </div>
+              </div>
+            </div>
+            <div className="container is-fluid">
+              <div className="columns">
+                {/* Hero Body */}
+                <div className="column is-6">
+                  <div className="content">
+                    <h2 className="">
+                      Cooper is the best way to <strong>share and collect</strong> money from friends and family!
+
+                      </h2>
+                    <p>Wether you're spliting the check for a pizza, the the movies or even a weekend trip,
+                    with Cooper you can send out a request for any amount and get rid of all the hassles.</p>
                   </div>
-                  {/* Hero Image */}
-                  <div className="column is-6">
-                    <figure className="image is-2by1">
-                      <img src="https://bulma.io/images/placeholders/640x320.png"></img>
-                    </figure>
-                  </div>
+                </div>
+                {/* Hero Image */}
+                <div className="column is-6">
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
                 </div>
               </div>
             </div>
@@ -42,27 +43,91 @@ export default class Home extends Component {
 
           {/* How-To */}
           <section className="hero is-medium is-bold">
-            <div className="hero-body">
-              <div className="container is-fluid">
-                <div className="columns">
-                  {/* Hero Image */}
-                  <div className="column is-6">
-                    <figure className="image is-2by1">
-                      <img src="https://bulma.io/images/placeholders/640x320.png"></img>
-                    </figure>
+            <div className="section">
+            <div className="content has-text-centered">
+                <div className="title">
+                  How does it work?
                   </div>
-                  {/* Hero Body */}
-                  <div className="column is-6">
-                    <div className="content">
-                      <h1 className="title">
-                        How does it work?
-                  </h1>
-                      <h4 className="">
-                        Step 1:
+                  <div className="subtitle">3 steps and done</div>
+              </div>
+            </div>
+            <div className="container is-fluid">
+              <div className="columns">
+                {/* Hero Image */}
+                <div className="column is-4">
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                  <div className="content">
+                    <h4 className="">
+                      Create a request
                       </h4>
-                      <p> Create a request.</p>
-                    </div>
                   </div>
+                </div>
+                {/* Hero Body */}
+                <div className="column is-4">
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                  <div className="content">
+                    <h4 className="">
+                      Send request to your friends
+                      </h4>
+                  </div>
+                </div>
+                <div className="column is-4">
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                  <div className="content">
+                    <h4 className="">
+                      Split it up!
+                      </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          {/* Partners */}
+          <section className="hero is-medium is-bold">
+            <div className="section">
+              <div className="content has-text-centered">
+                <div className="title">
+                  Meet our partners
+                  </div>
+                  <div className="subtitle">Get special offers from our partners!</div>
+              </div>
+            </div>
+            <div className="container is-fluid">
+              <div className="columns">
+                {/* Partner 1 */}
+                <div className="column is-3">
+
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                </div>
+                {/* Partner 1 */}
+                <div className="column is-3">
+
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                </div>
+                {/* Partner 1 */}
+                <div className="column is-3">
+
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+                </div>
+                {/* Partner 1 */}
+                <div className="column is-3">
+                  <figure className="image is-2by1">
+                    <img src="https://bulma.io/images/placeholders/640x320.png"></img>
+                  </figure>
+
                 </div>
               </div>
             </div>
