f7ee4ce8d559412ea8489d3f7e54fb9657897a52
Neymgm <neymgm@mac.com>
2018-05-03 00:27:56 -0500

    Add new routes for account interface

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import { Route, Switch, BrowserRouter, browserHistory} from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Signin';
-import Account from "./components/Dashboard/Account";
+import AccountProfile from "./components/Dashboard/AccountProfile";
+import AccountPassword from "./components/Dashboard/AccountPassword";
 import Dashboard from "./components/Dashboard/Main";
 import InviteToPool from "./components/Dashboard/InviteToPool";
 import EditPoolDetail from "./components/Dashboard/EditPoolDetail.js";
@@ -19,7 +20,8 @@ class App extends Component {
                 <Route exact path="/" component={ Home } />
                 <Route exact path="/SignIn" component={ Login } />
                 <Route exact path="/App" component={ Dashboard } />
-                <Route exact path="/Account" component={ Account } />
+                <Route exact path="/AccountProfile" component={ AccountProfile } />
+                <Route exact path="/AccountPassword" component={ AccountPassword } />
                 <Route exact path="/Invite" component={ InviteToPool } />
                 <Route exact path="/EditPool" component={ EditPoolDetail } />
                 <Route exact path="/ChatRoom" component={ ChatRoomPage } />
