797347cf70eb7258e8359e7560475c94b6b44c88
Hermes <hermes.espinola@wizeline.com>
2018-05-04 18:03:20 -0500

    restart server when deploying

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -9,3 +9,6 @@ export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 cd $1 && \
 tar zxvf package.tgz -C . && \
 mv dist/package.json . && \
+npm install && \
+pm2 delete front 2> /dev/null && \
+pm2 --name front start npm -- start
